47379ff843c1271063210101174d4b62123c2594
Francisco Vallarino <fjvallarino@gmail.com>
2015-10-15 22:40:48 -0300

    Add common modules

---
+++ common/network.js
@@ -0,0 +1,26 @@
+module.exports = {
+  httpFetch: function(dest, opts) {
+    if (!opts) {
+      return fetch(dest);
+    }
+
+    setHeaders();
+    return fetch(dest, opts);
+
+    function setHeaders() {
+      var headerArray = opts.headers,
+      headers;
+
+      if (!headerArray) {return;}
+
+      headers = new Headers();
+
+      headerArray.forEach(function(header) {
+        var nameValue = header.split(":");
+        headers.append(nameValue[0], nameValue[1].replace(" ", ""));
+      });
+
+      opts.headers = headers;
+    }
+  }
+};
---
+++ common/platform.js
@@ -0,0 +1,17 @@
+module.exports = {
+  getCoreUrl() {
+    return "https://rvaserver2.appspot.com";
+  },
+  getViewerUrl() {
+    return "http://rvashow.appspot.com";
+  },
+  getOS() {
+    return process.platform;
+  },
+  getArch() {
+    return process.arch;
+  },
+  getInstallDir() {
+    return process.env[(module.exports.getOS() == "win32") ? "USERPROFILE" : "HOME"];
+  }
+};
