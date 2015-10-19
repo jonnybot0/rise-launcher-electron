de2d909eb26c99b8791257b1ae56a2ca513cd7c8
Tyler Johnson <mail@tyler-johnson.ca>
2015-10-19 10:39:54 -0400

    Add integration test for external logger

--- common/network.js
+++ common/network.js
@@ -2,27 +2,6 @@ var fetch = require("node-fetch");
 
 module.exports = {
   httpFetch: function(dest, opts) {
-    if (!opts) {
-      return fetch(dest);
-    }
-
-    setHeaders();
     return fetch(dest, opts);
-
-    function setHeaders() {
-      var headerArray = opts.headers,
-      headers;
-
-      if (!headerArray) {return;}
-
-      headers = new Headers();
-
-      headerArray.forEach(function(header) {
-        var nameValue = header.split(":");
-        headers.append(nameValue[0], nameValue[1].replace(" ", ""));
-      });
-
-      opts.headers = headers;
-    }
   }
 };
