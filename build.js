a4f1c4af89fa6fbb13901632dbfc88b8e1b32661
Francisco Vallarino <fjvallarino@gmail.com>
2015-10-30 21:23:14 -0300

    Change build process to generate tar.gz files

--- build.js
+++ build.js
@@ -2,6 +2,9 @@ var spawnSync= require("child_process").spawnSync,
 fs = require("fs"),
 archiver = require("archiver"),
 path = require("path"),
+gunzip = require("gunzip-maybe"),
+gzip = require("zlib").createGzip(),
+tar = require("tar-fs"),
 packager = require("electron-packager");
 
 var opts = {
@@ -28,28 +31,71 @@ packager(opts, function done (err, appPath) {
 });
 
 function zipBuilds() {
+  //var artifacts = [["linux-ia32", "lnx-32"], ["linux-x64", "lnx-64"], ["win32-ia32", "win-32"], ["win32-x64", "win-64"]];
+  var artifacts = [["win32-x64", "win-64"]];
+
   console.log("zipping builds");
-  zipBuild("linux-ia32", "lnx-32");
-  zipBuild("linux-x64", "lnx-64");
-  zipBuild("win32-ia32", "win-32");
-  zipBuild("win32-x64", "win-64");
+  
+  Promise.all(artifacts.map((art)=>{
+    return zipBuild(art[0], art[1]);
+  }))
+  .then(()=>{
+    console.log("Builds zipped");
+  })
+  .catch((err)=>{
+    console.log("Error zipping builds", err);
+  });
+}
 
-  function zipBuild(platform, zipName) {
-    var output = fs.createWriteStream(path.join(__dirname, "builds", "rvplayer-installer-" + zipName + ".zip")),
-    archive = archiver("zip");
+function zipBuild(platform, zipName) {
+  var input = path.join(__dirname, "builds", "installer-" + platform);
+  var resources = path.join(input, "resources");
+  var outputTar = path.join(__dirname, "builds", "rvplayer-installer-" + zipName + ".tar");
+  var outputGz = path.join(__dirname, "builds", "rvplayer-installer-" + zipName + ".tar.gz");
 
-    output.on("close", function() {
-      console.log("archiver finalized " + platform);
+  return renameFile(path.join(resources, "atom.asar"), path.join(resources, "atom.ren"))
+  .then(()=>{
+    return tarFolder(platform, zipName);
+  })
+  .then(()=>{
+    return gzipTar(zipName);
+  })
+  .then(()=>{
+    fs.unlinkSync(outputTar);
+  });
+
+  function renameFile(oldName, newName) {
+    return new Promise((resolve, reject)=>{
+      fs.rename(oldName, newName, (err)=>{
+        if(!err) {
+          resolve();
+        }
+        else {
+          reject({ message: "Error renaming file", error: err });
+        }
+      });
     });
+  }
 
-    archive.on("error", function(err) {
-        throw err;
+  function tarFolder(platform, zipName) {
+    return new Promise((resolve, reject)=>{
+      tar.pack(input)
+      .pipe(fs.createWriteStream(outputTar))
+      .on("close", resolve)
+      .on("error", reject);
     });
+  }
 
-    archive.pipe(output);
+  function gzipTar(zipName) {
+    var input = fs.createReadStream(outputTar);
+    var output = fs.createWriteStream(outputGz);
 
-    archive
-    .directory(path.join(__dirname, "builds", "installer-" + platform), "Installer")
-    .finalize();
+    return new Promise((resolve, reject)=>{
+      input
+      .pipe(gzip)
+      .pipe(output)
+      .on("close", resolve)
+      .on("error", reject);
+    });
   }
 }
