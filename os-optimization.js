171c5aaa8c6e9fb755f5d2582d03861fc4d3ca4f
Tyler Johnson <mail@tyler-johnson.ca>
2015-11-19 23:21:52 -0500

    Update os optimization

--- os-optimization.js
+++ os-optimization.js
@@ -1,74 +1,84 @@
 var platform = require("./common/platform.js"),
-childProcess = require("child_process");
+childProcess = require("child_process"),
+linuxCommands = {
+  disableScreenSaver: [
+    "gsettings set org.gnome.desktop.session idle-delay 0"
+  ],
+  disableSleep: [
+    "gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0"
+  ],
+  disablePowerIdle: [
+    "gsettings set org.gnome.settings-daemon.plugins.power idle-dim false"
+  ],
+  disableSystemUpdates: [
+    "sudo mv /usr/bin/update-notifier /usr/bin/update-notifier.real",
+    "sudo echo '#!/bin/sh' >> /usr/bin/update-notifier",
+    "sudo echo 'exit 0' >> /usr/bin/update-notifier"
+  ],
+  disableApport: [
+    "sudo echo ' # ' >>/etc/default/apport",
+    "sudo echo 'enabled=0' >>/etc/default/apport"
+  ],
+  removeRVPlayerCron: [
+    "sudo sed -i.bak '/rvplayer/d' /etc/crontab"
+  ]
+},
+
+windowsCommands = {
+  disableScreenSaver: [
+    "reg add \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v ScreenSaveActive /t REG_SZ /d 0 /f",
+    "reg add \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v ScreenSaverIsSecure /t REG_SZ /d 0 /f",
+    "reg delete \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v SCRNSAVE.EXE /f"
+  ],
+  setHighPerformance: [
+    "powercfg -SETACTIVE a1841308-3541-4fab-bc81-f71556f20b4a"
+  ],
+  setUnpluggedNeverTimeout: [
+    "powercfg -change -monitor-timeout-dc 0",
+    "powercfg -change -disk-timeout-dc 0",
+    "powercfg -change -standby-timeout-dc 0",
+    "powercfg -change -hibernate-timeout-dc 0",
+  ],
+  setPluggedInNeverTimeout: [
+    "powercfg -change -monitor-timeout-ac 0",
+    "powercfg -change -disk-timeout-dc 0",
+    "powercfg -change -standby-timeout-ac 0",
+    "powercfg -change -hibernate-timeout-ac 0"
+  ],
+  disableChromeHDPIScaling: [
+    "reg add \"HKCU\\Software\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers\" /v \"" + platform.getInstallerPath() + "\" /t REG_SZ /d \"~ HIGHDPIAWARE\""
+  ],
+  disableSecurityAlerts: [
+    "reg add \"HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\" /v EnableBalloonTips /t REG_DWORD /d 0 /f"
+  ],
+  disableOnScreenKeyboard: [
+    "reg add \"HKEY_CURRENT_USER\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Accessibility\" /v Configuration /t REG_SZ /d \"\" /f"
+  ]
+};
 
 function execSync(command) {
-  childProcess.execSync(command, { timeout: 2000 });
+  childProcess.execSync(command, {timeout: 2000});
 }
 
 function updateSettings() {
   if(platform.isWindows()) {
-    module.exports.updateWindowsSettings();
+    executeCommands(windowsCommands);
   }
   else {
-    module.exports.updateLinuxSettings();
+    executeCommands(linuxCommands);
   }
 }
 
-function updateWindowsSettings() {
-  module.exports.disableWindowsScreenSaver();
-  module.exports.updateWindowsPowerSettings();
-  module.exports.disableWindowsChromeHDPIScaling();
-  module.exports.disableWindowsSecurityAlerts();
-  module.exports.disableWindowsOnscreenKeyboard();
-}
-
-function disableWindowsScreenSaver() {
-  execSync("reg add \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v ScreenSaveActive /t REG_SZ /d 0 /f");
-  execSync("reg add \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v ScreenSaverIsSecure /t REG_SZ /d 0 /f");
-  execSync("reg delete \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v SCRNSAVE.EXE /f");
-}
-
-function updateWindowsPowerSettings() {
-  // Set the 'Power Management' to High performance
-  execSync("powercfg -SETACTIVE a1841308-3541-4fab-bc81-f71556f20b4a");
-
-  // Set the unplugged settings to 'Never'
-  execSync("powercfg -change -monitor-timeout-dc 0");
-  execSync("powercfg -change -disk-timeout-dc 0");
-  execSync("powercfg -change -standby-timeout-dc 0");
-  execSync("powercfg -change -hibernate-timeout-dc 0");
-
-  // Set the plugged in settings to 'Never'
-  execSync("powercfg -change -monitor-timeout-ac 0");
-  execSync("powercfg -change -disk-timeout-dc 0");
-  execSync("powercfg -change -standby-timeout-ac 0");
-  execSync("powercfg -change -hibernate-timeout-ac 0");
-}
-
-function disableWindowsChromeHDPIScaling() {
-  execSync("reg add \"HKCU\\Software\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers\" /v \"" + platform.getInstallerPath() + "\" /t REG_SZ /d \"~ HIGHDPIAWARE\"");
-}
-    
-function disableWindowsSecurityAlerts() {
-  execSync("reg add \"HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\" /v EnableBalloonTips /t REG_DWORD /d 0 /f");
-}
-
-function disableWindowsOnscreenKeyboard() {
-  execSync("reg add \"HKEY_CURRENT_USER\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Accessibility\" /v Configuration /t REG_SZ /d \"\" /f");
-}
-
-function updateLinuxSettings() {
-
+function executeCommands(osCommands) {
+  Object.keys(osCommands).forEach((key)=>{
+    osCommands[key].forEach((command)=>{
+      execSync(command);
+    });
+  });
 }
 
 module.exports = {
-  execSync,
-  updateSettings,
-  updateWindowsSettings,
-  disableWindowsScreenSaver,
-  updateWindowsPowerSettings,
-  disableWindowsChromeHDPIScaling,
-  disableWindowsSecurityAlerts,
-  disableWindowsOnscreenKeyboard,
-  updateLinuxSettings
+  linuxCommands,
+  windowsCommands,
+  updateSettings
 };
