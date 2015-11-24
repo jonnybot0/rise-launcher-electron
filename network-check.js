12093fccf6de6b0836d195ccae6659d12df38807
Tyler Johnson <mail@tyler-johnson.ca>
2015-11-24 11:46:11 -0500

    Add network checks for required sites

---
+++ network-check.js
@@ -0,0 +1,25 @@
+var network = require("./common/network.js"),
+siteList = [
+  "http://rvashow2.appspot.com",
+  "http://rvashow.appspot.com",
+  "http://rvaserver2.appspot.com",
+  "http://storage-dot-rvaserver2.appspot.com",
+  "http://store.risevision.com",
+  "http://commondatastorage.googleapis.com",
+  "http://googleapis.com",
+  "http://accounts.google.com",
+  "http://talkgadget.google.com",
+  "http://s3.amazonaws.com",
+  "http://p.jwpcdn.com/6/5/jwpsrv.js",
+  "http://rackcdn.com"
+];
+
+module.exports = {
+  checkSites() {
+    var siteConnections = siteList.map((site)=>{
+      return network.httpFetch(site);
+    });
+
+    return Promise.all(siteConnections);
+  }
+};
