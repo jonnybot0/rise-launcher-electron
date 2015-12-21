e9010d9a99f4eecf8929ce353fd23bc5c7a73d9f
Tyler Johnson <mail@tyler-johnson.ca>
2015-12-21 15:36:46 -0500

    Retry network checks

--- network-check.js
+++ network-check.js
@@ -14,35 +14,52 @@ siteList = [
   "http://p.jwpcdn.com/6/5/jwpsrv.js"
 ];
 
+function checkSite(site, retryCount, timeout) {
+  retryCount = retryCount === undefined ? 5 : retryCount;
+
+  return network.httpFetch(site, {timeout: timeout || 5000})
+  .then((res)=>{
+    if (res.status < 200 || res.status > 299) {
+      log.external("network prereq error", res.status + " - " + site);
+      if (retryCount === 0) { throw new Error("not ok");}
+      return platform.waitForMillis(timeout || 5000)
+      .then(checkSite.bind(null, site, retryCount - 1, timeout));
+    }
+    promisesPct += 9;
+    log.all("Checking network connectivity - " + site, "", promisesPct + "%");
+  })
+  .catch((err)=>{
+    console.log("error found" + retryCount);
+    log.external("network prereq error", err);
+    if (retryCount === 0) { throw new Error("not ok");}
+    return platform.waitForMillis(timeout || 5000)
+    .then(checkSite.bind(null, site, retryCount - 1, timeout));
+  });
+}
+
 module.exports = {
-  checkSitesWithElectron() {
+  checkSitesWithElectron(retryCount, timeout) {
     promisesPct = 0;
     var siteConnections = siteList.map((site)=>{
-      return network.httpFetch(site, {timeout: 9000})
-      .then((res)=>{
-        if (res.status < 200 || res.status > 299) {
-          log.external("network prereq error", res.status + " - " + site);
-          throw new Error("not ok");
-        }
-        promisesPct += 9;
-        log.all("Checking network connectivity - " + site, "", promisesPct + "%");
-      })
-      .catch((err)=>{
-        log.external("network prereq error", err);
-        throw new Error("not ok");
-      });
+      return checkSite(site, retryCount, timeout);
     });
 
     return Promise.all(siteConnections);
   },
-  checkSitesWithJava() {
+  checkSitesWithJava(retryCount, timeout) {
     var command = `${platform.getJavaExecutablePath()} ${network.getJavaProxyArgs().join(" ")} -jar ${path.join(platform.getInstallerDir(), "resources", "app", "java-network-test.jar")} ${siteList.join(" ")}`;
+
+    retryCount = retryCount === undefined ? 5 : retryCount;
 
     if(platform.isDevMode()) return Promise.resolve();
 
     return platform.spawn(command)
     .then((retCode)=>{
-      if (retCode !== 0) {throw new Error(retCode);}
+      if (retCode !== 0) {
+        if (retryCount === 0) {throw new Error(retCode);}
+        return platform.waitForMillis(timeout || 5000)
+        .then(module.exports.checkSitesWithJava.bind(null, retryCount - 1, timeout));
+      }
       return retCode;
     });
   }
