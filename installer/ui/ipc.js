dd108f05611c064997fd05cd15aec6b909ad9aab
Francisco Vallarino <fjvallarino@gmail.com>
2016-02-16 12:01:24 -0300

    Show/hide requires reboot message on UI startup to properly handle main window

--- installer/ui/ipc.js
+++ installer/ui/ipc.js
@@ -1,11 +1,17 @@
 const ipc = require("electron").ipcRenderer;
 
-ipc.on("first-ping", ()=> {
+ipc.on("first-ping", (evt, detail)=> {
   ipc.send("ui-pong");
   var closeButton = document.querySelector("#close"),
   cancelButton = document.querySelector("#cancel"),
   addToStartup = document.querySelector("#addToStartup"),
-  continueButton = document.querySelector("#continue");
+  continueButton = document.querySelector("#continue"),
+  restartBlocks = document.querySelectorAll(".restartBlock"),
+  requiresReboot = detail.isWindows && detail.version !== "7";
+
+  for(var i = 0; i < restartBlocks.length; i++) {
+    restartBlocks[i].style.display = requiresReboot ? "block" : "none";
+  }
 
   closeButton.addEventListener("click", ()=>{ipc.send("close");});
   cancelButton.addEventListener("click", ()=>{ipc.send("close");});
@@ -76,20 +82,14 @@ ipc.on("start-unattended", ()=> {
   installingSlide.className = "container slide active";
 });
 
-ipc.on("start-unattended-countdown", (evt, detail)=> {
+ipc.on("start-unattended-countdown", ()=> {
   var currentSlide = document.querySelector(".container.slide.active"),
   unattendedCountdownSlide = document.querySelector("#unattendedCountdown"),
-  playerInstallationHeader = document.querySelector("#playerInstallationHeader"),
-  restartBlocks = document.querySelectorAll(".restartBlock"),
-  requiresReboot = detail.isWindows && detail.version !== "7";
+  playerInstallationHeader = document.querySelector("#playerInstallationHeader");
 
   currentSlide.className = "container slide inactive";
   unattendedCountdownSlide.className = "container slide active";
-  playerInstallationHeader.style.display = !requiresReboot ? "block" : "none";
-
-  for(var i = 0; i < restartBlocks.length; i++) {
-    restartBlocks[i].style.display = requiresReboot ? "block" : "none";
-  }
+  playerInstallationHeader.style.display = "none";
   
   ipc.send("install-unattended");
 });
